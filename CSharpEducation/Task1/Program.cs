fce5657eb0897529fcdab3298eabcecb9c7682b1
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-02 13:28:20 +0400

    Домашнее задание №1
    Добавил в репозиторий первое домашнее задание - решение CSharpEducation, проект Task1. Выгрузил все в главную ветку (т. к. оно уже проверено и принято).

---
+++ CSharpEducation/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Security;
+                                                     //Подключение библиотеки "System"
+
+namespace CSharpEducation                            //Пространство имен, название "CSharpEducation" как имя папки проекта (не обязательно?)
+{
+    class Program                                    //Создаем класс, название класса должно быть таким же как название файла!
+    {
+
+        static void Main()                           //Пишем функцию с названием "Main"
+        {
+            Console.Write("Hello, World!");
+        }
+
+    }
+
+}
