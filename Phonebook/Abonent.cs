cf5fb2362daaf98a8db04d2f5ed89b26c5fdebee
Andrew-1809 <fedandrey93@yandex.ru>
2025-05-26 21:06:49 +0400

    Домашнее задание №3 (Phonebook)
    Создано решение Phonebook. Внутри решения создано одноименное консольное приложение. Создан класс "Abonent".

---
+++ Phonebook/Abonent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Abonent
+    {
+        public string Name { get; set; }
+
+        private string phone;
+        public string Phone 
+        {
+            get 
+            {  
+                return phone;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(phone))
+                    throw new ArgumentNullException("Абонент с таким нормером уже существует!");
+
+                phone = value;
+            }
+        }
+        public Abonent(string name, string phone) 
+        {
+            this.Phone = phone;
+            this.Name = name;
+        }
+    }
+}
